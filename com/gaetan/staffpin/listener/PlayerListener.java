ac8ed563ef8a24f0db07c2eed331f2be95f9d3af
Gaetan <gaetanfaucher1301@gmail.com>
2021-02-04 14:20:10 +0100

    Patch all versions

--- com/gaetan/staffpin/listener/PlayerListener.java
+++ com/gaetan/staffpin/listener/PlayerListener.java
@@ -42,7 +42,7 @@ public final class PlayerListener implements Listener {
     /**
      * When a player with the pin permission join the server
      */
-    @EventHandler(priority = EventPriority.LOWEST)
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onJoin(final PlayerJoinEvent event) {
         final Player player = event.getPlayer();
 
